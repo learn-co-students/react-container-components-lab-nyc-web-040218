4d71d52ae75ac3c7e833fb280eeae56ced7e99ae
briecodes <hello@brie.work>
2018-06-07 18:08:02 -0400

    progress..

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -9,88 +9,42 @@ const NYT_API_KEY = '81ce09e59d3e45c2a3d61aed433d92cc';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// const myObj = [];
-// let doc = [];
-// function loadHtml(){
-//     doc = myObj.map((movie, index) => {
-//         return <MovieReviews key={index} movie={movie} />
-//     })
-// }
-// doc = this.state.movies.map((movie, index) => {
-//     return <MovieReviews key={index} movie={movie} />
-// });
-
-// fetch(URL).then( response => response.json() ).then(array => {
-//     for (const item in array.results){
-//         myObj.push({
-//             title: array.results[item].display_title,
-//             opening_date: array.results[item].opening_date,
-//             byline: array.results[item].byline,
-//             headline: array.results[item].headline,
-//             summary: array.results[item].summary_short,
-//             mmUrl: array.results[item].multimedia["src"],
-//             mpaa_rating: array.results[item].mpaa_rating
-//         })
-//     }
-// }).then( loadHtml() )
-
 
 class LatestMovieReviewsContainer extends React.Component {
     state = {
-        movies: [{title: "Loading movies..."}]
-    }
-    
-    myObj = [];
-
-    otherObj = [{title: "...still loading..."}]
-
-    fetchData = () => {
-        fetch(URL).then(response => response.json()).then(array => {
-            for (const item in array.results) {
-                this.myObj.push({
-                    title: array.results[item].display_title,
-                    opening_date: array.results[item].opening_date,
-                    byline: array.results[item].byline,
-                    headline: array.results[item].headline,
-                    summary: array.results[item].summary_short,
-                    mmUrl: array.results[item].multimedia["src"],
-                    mpaa_rating: array.results[item].mpaa_rating
-                })
-            }
-        })
+        movies: []
     }
 
-    // gettingReviews() {
-    //     fetch(URL)
-    //         .then(res => res.json())
-    //         .then(movies => this.setState({ movies: movies.results }));
-    // }
 
     componentWillMount() {
         fetch(URL)
             .then(res => res.json())
-            .then(movies => this.setState({ movies: movies.results }));
-    }
-
-    logThis = () => {
-        // console.log(this.myObj)
-        this.setState({
-            movies: this.myObj
-        })
-    }
-    checkaDis = () => {
-        console.log(this.state.movies)
+            .then(res => {
+                for (const item in res.results) {
+                    this.setState(prevState => ({
+                        movies: [...prevState.movies, {
+                            title: res.results[item].display_title,
+                            opening_date: res.results[item].opening_date,
+                            byline: res.results[item].byline,
+                            headline: res.results[item].headline,
+                            summary: res.results[item].summary_short,
+                            mmUrl: res.results[item].multimedia["src"],
+                            mpaa_rating: res.results[item].mpaa_rating
+                        }]
+                      }))
+                    }
+            })
     }
 
     render() {
+        
         let moviesStructure = this.state.movies.map((movie, index) => {
                 return <MovieReviews key={index} movie={movie} />
-            })
+            });
+
         return (
             <div>
-                <h1 onClick={this.fetchData} >Load Now ></h1>
-                <h1 onClick={this.logThis} >double check></h1>
-                <h1 onClick={this.checkaDis} >triple check check></h1>
+                <h1 onClick={this.fetchData} >Movies</h1>
                 { moviesStructure }
             </div>
         )
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,13 +3,17 @@ import React from "react";
 class MovieReviews extends React.Component {
     render() {
         return (
-            <div className="review-list">
-                <div className="review">
-                    <h4>{this.props.movie.title}</h4>
-                </div>
+            <div className="review">
+                <h4>{this.props.movie.headline}</h4>
+                <h5>By: {this.props.movie.byline}</h5>
+
+                <p>
+                    {this.props.movie.title}, Rated: {this.props.movie.mpaa_rating}, Opens: {this.props.movie.opening_date}<br>
+                </p>
+                <p>{this.props.movie.summary}</p>
             </div>
         )
     }
 }
 
-export default MovieReviews;
+export default MovieReviews
