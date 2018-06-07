326aaad1d177a5b675d176f4c5933e33ab0284d2
William Dale <williambdale@gmail.com>
2018-06-07 17:53:26 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
 import 'isomorphic-fetch';
+require('es6-promise').polyfill();
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listings: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${URL}`)
+      .then(res => res.json())
+      .then(listings => this.setState({
+        listings: listings.results
+      }))
+  }
+
+  render() {
+    return (
+      <MovieReviews MovieListings={this.state.listings} />
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+  return (
+    <ol>
+      {props.MovieListings.map(movie => {
+        return <li>Title: {movie.display_title}, Rating: {movie.mpaa_rating}</li>
+      })}
+    </ol>
+  )
+}
+
+export default MovieReviews;
