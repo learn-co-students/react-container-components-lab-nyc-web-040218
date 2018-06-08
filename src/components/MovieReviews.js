7339c87f8c767123bc0184a1c274a8dba326e19a
briecodes <hello@brie.work>
2018-06-08 13:09:06 -0400

    MR refactored into stateless function component.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,19 +1,13 @@
 import React from "react";
 
-class MovieReviews extends React.Component {
-    render() {
-        return (
-            <div className="review">
-                <h4>{this.props.review.headline}</h4>
-                <h5>By: {this.props.review.byline}</h5>
-
-                <p>
-                    {this.props.review.title}, Rated: {this.props.review.mpaa_rating}, Opens: {this.props.review.opening_date}
-                </p>
-                <p>{this.props.review.summary}</p>
-            </div>
-        )
-    }
-}
+const MovieReviews = ({review}) =>
+    <div className="review">
+        <h4>{review.headline}</h4>
+        <h5>By: {review.byline}</h5>
+        <p>
+            {review.title}, Rated: {review.mpaa_rating}, Opens: {review.opening_date}
+        </p>
+        <p>{review.summary}</p>
+    </div>;
 
 export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,14 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from "./MovieReviews"
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+class SearchableMovieReviewsContainer extends React.Component {
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+            </div>
+        )
+    }
+}
