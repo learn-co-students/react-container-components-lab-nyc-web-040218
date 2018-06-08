2a004c9ddef8b621113d7e7312fbb159d2b4974a
yongnicholaskim <yongnicholaskim@gmail.com>
2018-06-08 15:17:02 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,16 +1,28 @@
 // Code MovieReviews Here
 import React from 'react';
-import LatestMovieReviewsContainer from './LatestMovieReviewsContainer.js';
-import SearchableMovieReviewsContainer from './SearchableMovieReviewsContainer.js';
 
-const MovieReviews = props => {
-  return (
-    <div className="review-list">
-      <p>
-        {}
-      </p>
+const Review = ({
+    headline,
+    byline,
+    link,
+    summary_short
+  }) => {return (
+    <div key={headline} className="review">
+      <header>
+        <a className="review-link" href={link.url}>{headline}</a>
+        <br />
+        <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
     </div>
-  )
-}
+  );
+};
+
+const MovieReviews = ({ reviews }) => 
+  <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
 
 export default MovieReviews;
