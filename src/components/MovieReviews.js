f90ef75a38e6737f5891d98688f7e17f968fee19
Daniel Brazel <dbrazel4@gmail.com>
2018-06-11 17:55:00 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const MovieReviews = (props) => {
+  return(
+    <div className='review-list'>
+      {props.reviews.map(review => {
+        return(
+          <div key={review.headline} className="review">
+            <ul>
+              <li><h4>{review.headline}</h4></li>
+              <li>By: {review.byline}</li>
+              <li>Summary: {review.summary_short}</li>
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+MovieReviews.defaultProps = {reviews: []}
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,47 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}&query=`;
+
+export default class SearchableMovieReviewsContainer extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  handleSubmit = e => {
+
+    e.preventDefault()
+
+    fetch(URL + this.state.searchTerm)
+    .then(res => res.json())
+    .then(resp => this.setState({reviews: resp.results}))
+  }
+
+  logger = e => {e.preventDefault()
+    console.log(this.state.searchTerm)
+  }
+
+  handleChange = e => this.setState({searchTerm: e.target.value})
+
+  render(){
+    return(
+      <div className='searchable-movie-reviews'><h3>Search our Movie Reviews:</h3>
+        <form>
+          <input placeholder='Input Search Term' onChange={this.handleChange}></input>
+          <button type='submit'onClick={this.handleSubmit}>Submit</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
